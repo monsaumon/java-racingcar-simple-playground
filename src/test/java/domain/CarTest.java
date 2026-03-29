33225d9566f8e1e5fa47943b1b5e530da4d45c57
monsaumon <monsaumon@naver.com>
2026-03-30 02:01:41 +0900

    test: 범위를 벗어나는 숫자 테스트 추가

--- src/test/java/domain/CarTest.java
+++ src/test/java/domain/CarTest.java
@@ -1,6 +1,7 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -8,20 +9,32 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
     @DisplayName("입력이 4 이상이면 1, 아니면 0을 반환한다.")
     @ParameterizedTest
     @CsvSource(value = {"0,0", "3,0", "4,1", "9,1"})
-    public void testMoveIfGreaterThan(final int n, final int expected) {
+    public void testMoveIfGreaterThan_ValidNumber(final int number, final int expected) {
         // given
         final Car car = new Car("asdf");
 
         // when
-        final int actual = car.moveIfGreaterThan(n);
+        final int actual = car.moveIfGreaterThan(number);
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("입력이 0보다 작거나 10 이상이면 IllegalArgumentException을 throw한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-10, -1, 10, 20})
+    public void testMoveIfGreaterThan_InvalidNumber(final int number) {
+        // given
+        final Car car = new Car("asdf");
+
+        // when & then
+        assertThatThrownBy(() -> car.moveIfGreaterThan(number)).isInstanceOf(IllegalArgumentException.class).hasMessage("입력값이 범위를 벗어났습니다.");
     }
 
     @DisplayName("입력 배열의 각 원소마다 4 이상이면 움직이고, 이동 내역을 반환한다.")
