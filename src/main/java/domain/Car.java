1e7cd727aa465c5728fda88aaaf15d37fcd978c8
monsaumon <monsaumon@naver.com>
2026-03-30 02:01:14 +0900

    feat: moveIfGreaterThan의 입력값이 범위 안에 있는지 확인

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -6,14 +6,23 @@ import java.util.List;
 import static domain.Utils.validateName;
 
 public record Car(String name) {
-    private static final int MOVING_THRESHOLD = 4;
+    private static final int MOVING_THRESHOLD = 3;
+    private static final int NUMBER_LOWER_BOUND = 0;
+    private static final int NUMBER_UPPER_BOUND = 10;
 
     public Car(final String name) {
         this.name = validateName(name);
     }
 
+    private void checkNumberBound(final int number) {
+        if (number < NUMBER_LOWER_BOUND || number >= NUMBER_UPPER_BOUND) {
+            throw new IllegalArgumentException("입력값이 범위를 벗어났습니다.");
+        }
+    }
+
     public int moveIfGreaterThan(final int number) {
-        return Boolean.compare(number >= MOVING_THRESHOLD, false);
+        checkNumberBound(number);
+        return Boolean.compare(number > MOVING_THRESHOLD, false);
     }
 
     public int[] generateMoveHistory(final int[] numbers) {
