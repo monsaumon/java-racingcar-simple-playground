8494200c107f10444fa969a2b157a2ef258fe4dd
monsaumon <monsaumon@naver.com>
2026-03-30 00:43:21 +0900

    feat: 입력받은 length가 0보다 큼을 검증하는 부분 추가

--- src/main/java/domain/RandomArrayGenerator.java
+++ src/main/java/domain/RandomArrayGenerator.java
@@ -11,7 +11,14 @@ public class RandomArrayGenerator {
         this.random = new Random();
     }
 
+    private void validateLength(final int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("배열의 길이는 0 이하일 수 없습니다.");
+        }
+    }
+
     public int[] generateRandomArray(final int length) {
+        validateLength(length);
         final int[] array = new int[length];
         for (int i = 0; i < length; i++) {
             array[i] = random.nextInt(RANDOM_INT_BOUND);
@@ -20,6 +27,8 @@ public class RandomArrayGenerator {
     }
 
     public int[][] generateRandom2DArray(final int length1, final int length2) {
+        validateLength(length1);
+        validateLength(length2);
         final int[][] array = new int[length1][length2];
         for (int i = 0; i < length1; i++) {
             array[i] = generateRandomArray(length2);
